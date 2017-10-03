cdea37901356e3a95309dfe85ec70ca8b3c2c02a
David Cardoso <David@Davids-MacBook-Pro.local>
2017-10-03 19:13:44 -0300

    Share with other apps

--- MapsExemplo/app/src/main/java/com/fiap/felipevieira/mapsexemplo/CheckInActivity.java
+++ MapsExemplo/app/src/main/java/com/fiap/felipevieira/mapsexemplo/CheckInActivity.java
@@ -1,14 +1,18 @@
 package com.fiap.felipevieira.mapsexemplo;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.io.ByteArrayOutputStream;
 import java.io.Serializable;
 
 public class CheckInActivity extends AppCompatActivity {
@@ -16,7 +20,7 @@ public class CheckInActivity extends AppCompatActivity {
     private Usuario usuario;
     private ImageView imgCheckin;
     private Intent ret;
-
+    private Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +37,7 @@ public class CheckInActivity extends AppCompatActivity {
             Log.i("NATURA_POINTS", "*CAPTURAR CAMINHO IMAGEM - " + uri);
 
             BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-            Bitmap bitmap = BitmapFactory.decodeFile(uri, bmOptions);
+            bitmap = BitmapFactory.decodeFile(uri, bmOptions);
 
             imgCheckin.setImageBitmap(bitmap);
             usuario.setPontos(usuario.getPontos() + 5);
@@ -51,11 +55,31 @@ public class CheckInActivity extends AppCompatActivity {
         finish();
     }
 
+    public void share(View view) {
+
+        Intent i = new Intent(Intent.ACTION_SEND);
+
+        i.setType("image/*");
+
+        i.putExtra(Intent.EXTRA_STREAM, getImageUri(this, bitmap));
+        try {
+            startActivity(Intent.createChooser(i, "My Profile ..."));
+        } catch (android.content.ActivityNotFoundException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
+    }
 
     @Override
     public void finish() {
 
         super.finish();
     }
-
 }
